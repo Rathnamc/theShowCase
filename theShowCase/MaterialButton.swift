24236f374743b3bc607fb12a90b7cdf9f416aea5
Christopher Rathnam <Rathnam.C@gmail.com>
2016-03-19 23:57:39 -0400

    Added Facebook Pod, set up IBdesignables

--- theShowCase/MaterialButton.swift
+++ theShowCase/MaterialButton.swift
@@ -7,15 +7,73 @@
 //
 
 import UIKit
+import pop
 
+@IBDesignable
 class MaterialButton: UIButton {
-
+    
+    
+    
+    @IBInspectable var cornerRadius: CGFloat = 5.0 {
+        didSet {
+            setUpView()
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: CGFloat = 1.0 {
+        didSet {
+            setUpView()
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 5.0 {
+        didSet {
+            setUpView()
+        }
+    }
+    
     override func awakeFromNib() {
-        layer.cornerRadius = 5.0
-        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
-        layer.shadowOpacity = 0.8
-        layer.shadowRadius = 5.0
-        layer.shadowOffset = CGSizeMake(0.0, 2.0)
+        setUpView()
+        
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setUpView()
     }
 
+    func setUpView () {
+        
+        layer.cornerRadius = cornerRadius
+        layer.shadowOpacity = 1.0
+        layer.shadowRadius = 5.0
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
+        
+        self.addTarget(self, action: "scaleToSmall", forControlEvents: .TouchDown)
+        self.addTarget(self, action: "scaleToSmall", forControlEvents: .TouchDragEnter)
+        self.addTarget(self, action: "scaleAnimation", forControlEvents: .TouchUpInside)
+        self.addTarget(self, action: "scaleDefault", forControlEvents: .TouchDragExit)
+    }
+    
+    func scaleToSmall() {
+        let scaleAnim = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY)
+        scaleAnim.toValue = NSValue(CGSize: CGSizeMake(0.80, 0.80))
+        self.layer.pop_addAnimation(scaleAnim, forKey: "layerScaleSmallAnimation")
+    }
+    
+    func scaleAnimation() {
+        let scaleAnim = POPSpringAnimation(propertyNamed: kPOPLayerScaleXY)
+        scaleAnim.velocity = NSValue(CGSize: CGSizeMake(3.0, 3.0))
+        scaleAnim.toValue = NSValue(CGSize: CGSizeMake(1.0, 1.0))
+        scaleAnim.springBounciness = 20
+        self.layer.pop_addAnimation(scaleAnim, forKey: "layerScaleSmallAnimation")
+    }
+    
+    func scaleDefault() {
+        let scaleAnim = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY)
+        scaleAnim.toValue = NSValue(CGSize: CGSizeMake(1, 1))
+        self.layer.pop_addAnimation(scaleAnim, forKey: "layerScaleSmallAnimation")
+    }
+
+    
 }
--- theShowCase/MaterialTextField.swift
+++ theShowCase/MaterialTextField.swift
@@ -8,22 +8,43 @@
 
 import UIKit
 
+@IBDesignable
 class MaterialTextField: UITextField {
 
+    @IBInspectable var inset: CGFloat = 0
+    
+    @IBInspectable var borderWidth: CGFloat = 1.0 {
+        didSet {
+            setupView()
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 5.0 {
+        didSet {
+            setupView()
+        }
+    }
+    
+    
+    
     override func awakeFromNib() {
-        layer.cornerRadius = 2.0
-        layer.borderColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
-        layer.borderWidth = 1.0
+        setupView()
     }
     
     //For placeholder
     override func textRectForBounds(bounds: CGRect) -> CGRect {
-        return CGRectInset(bounds, 10, 0)
+        return CGRectInset(bounds, inset, inset)
     }
     
     //for Editable text
     override func editingRectForBounds(bounds: CGRect) -> CGRect {
-        return CGRectInset(bounds, 10, 0)
+        return textRectForBounds(bounds)
+    }
+    
+    func setupView() {
+        layer.cornerRadius = cornerRadius
+        layer.borderColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
+        layer.borderWidth = borderWidth
     }
 
 }
